a211e6f336c6a3b85fe8c1785503e089431aacf8
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-07-19 08:23:35 +0700

    Add: run main without class Program

---
+++ RunMe.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace csharp_learning
+{
+    class RunMe
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Run Main Without Class Program");
+        }
+    }
+}
