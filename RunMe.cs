42c0ef7d88398f47482c3c0d4ec4e8ac2a977ea9
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-07-21 22:00:43 +0700

    Edit: Main for add two number and show result with formatString

--- RunMe.cs
+++ RunMe.cs
@@ -6,9 +6,14 @@ namespace csharp_learning
     {
         static void Main(string[] args)
         {
-            string firstWord = Console.ReadLine();
-            string secondWord = Console.ReadLine();
-            Console.WriteLine(firstWord + secondWord);
+            Console.Write("Enter the first number: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Result Of {0}+{1}= {2}",
+                firstNumber, secondNumber, firstNumber + secondNumber);
         }
     }
 }
